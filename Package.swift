0d5eedcc4c94c213407b475fe77c4bfabce0f823
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-12 20:15:09 +0100

    Convert dependencies to remote URLs

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(path: "../swift-w3c-epub"),
+        .package(url: "https://github.com/swift-standards/swift-w3c-epub.git", from: "0.0.1"),
     ],
     targets: [
         .target(
